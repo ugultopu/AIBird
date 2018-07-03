8aa650507997e48f5bf5c30744569a4a8a5254eb
Utku Gultopu <ugultopu@gmail.com>
2018-07-03 18:32:34 -0300

    Run the script with 'nohup' instead of commands

--- generate-structures.sh
+++ generate-structures.sh
@@ -9,10 +9,10 @@ mkdir -p "AIBird/$directory"
 
 # Run PonyGE2 script
 cd PonyGE2/src
-nohup python3 ponyge.py --parameters ai_birds.txt --file_path "$directory" &> "../$directory/output.txt" &
+python3 ponyge.py --parameters ai_birds.txt --file_path "$directory" &> "../$directory/output.txt"
 # Run the script with 'nohup' in the background. Pass the generated directory's
 # path to the script. Also pass 'positions.txt' from the last run of PonyGE2
 cd ../..
-nohup python3 AIBird/src/python/enumerate-structures.py "AIBird/$directory" "PonyGE2/$directory/positions.txt" &> "AIBird/$directory/output.txt" &
+python3 AIBird/src/python/enumerate-structures.py "AIBird/$directory" "PonyGE2/$directory/positions.txt" &> "AIBird/$directory/output.txt" &
 # Save the PID number of the script to a file
 echo $! > "AIBird/$directory/ai_birds.pid"

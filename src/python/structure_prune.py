fd4ba374ac13619753ad35cba9e5abed661eb33c
Utku Gultopu <ugultopu@gmail.com>
2018-08-03 15:18:39 -0300

    Skip the first row and column of CSV file
    Skipped them as done in MATLAB implementation.

--- src/python/structure_prune.py
+++ src/python/structure_prune.py
@@ -3,7 +3,8 @@ from csv import reader
 def structure_prune(k):
     with open('export_2018_08_01-14_28_49.csv') as f:
         export_file_reader = reader(f)
+        next(export_file_reader)
         for row in export_file_reader:
-            print(row)
+            print(row[1:])
 
 structure_prune(0)

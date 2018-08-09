8b67df822506da56a4aebebae5a9e72ff1f8ca69
Utku Gultopu <ugultopu@gmail.com>
2018-08-08 21:38:19 -0300

    Pad NumPy arrays
    Pad NumPy arrays with 0 until the length of the longest array in
    'column_vectors'.

--- src/python/structure_prune.py
+++ src/python/structure_prune.py
@@ -1,13 +1,15 @@
 import logging as log
+import numpy as np
 
-from numpy import array
 from pprint import pformat
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin_min
 
 def structure_prune(k, column_vectors):
     log.info(f'column vectors are:\n{pformat(column_vectors)}')
-    vectors_as_numpy_array = array(column_vectors)
+    max_row_length = len(max(column_vectors, key=len))
+    for idx, vector in enumerate(column_vectors): column_vectors[idx] = np.pad(vector, (0, max_row_length - len(vector)), 'constant')
+    vectors_as_numpy_array = np.array(column_vectors)
     log.info(f'column vectors as numpy array are:\n{pformat(vectors_as_numpy_array)}')
     kmeans = KMeans(n_clusters=k).fit(vectors_as_numpy_array)
     return ( pairwise_distances_argmin_min(kmeans.cluster_centers_, vectors_as_numpy_array),

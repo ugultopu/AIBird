5f55d598a149838a36272e45150fc0ce99490b3a
Utku Gultopu <ugultopu@gmail.com>
2018-10-04 12:00:35 -0300

    Rename "positions.txt" to "silhouette.txt"

--- src/shell/generate-structures-with-given-silhouette.sh
+++ src/shell/generate-structures-with-given-silhouette.sh
@@ -2,13 +2,13 @@
 # Run this script from one level above PonyGE2 and AIBird projects. For that,
 # symlink this script to one level above PonyGE2 and AIBird projects
 
-# The path to the file that contains the positions
-positions="$1"
+# The path to the file that contains the silhouette
+silhouette="$1"
 
 # Create output directories named after current date and time
 directory="results/$(date '+%Y_%m_%d-%H_%M_%S')"
 mkdir -p "AIBird/$directory"
 
-python3 AIBird/src/python/enumerate-structures.py "AIBird/$directory" "$positions" &> "AIBird/$directory/output.txt" &
+python3 AIBird/src/python/enumerate-structures.py "AIBird/$directory" "$silhouette" &> "AIBird/$directory/output.txt" &
 # Save the PID number of the script to a file
 echo $! > "AIBird/$directory/ai_birds.pid"
--- src/shell/generate-structures.sh
+++ src/shell/generate-structures.sh
@@ -10,9 +10,9 @@ mkdir -p "AIBird/$directory"
 # Run PonyGE2 script
 cd PonyGE2/src
 python3 ponyge.py --parameters ai_birds.txt --file_path "$directory" &> "../$directory/output.txt"
-# Pass the generated directory's path to the script. Also pass 'positions.txt'
+# Pass the generated directory's path to the script. Also pass 'silhouette.txt'
 # from the last run of PonyGE2
 cd ../..
-python3 AIBird/src/python/enumerate-structures.py "AIBird/$directory" "PonyGE2/$directory/positions.txt" &> "AIBird/$directory/output.txt" &
+python3 AIBird/src/python/enumerate-structures.py "AIBird/$directory" "PonyGE2/$directory/silhouette.txt" &> "AIBird/$directory/output.txt" &
 # Save the PID number of the script to a file
 echo $! > "AIBird/$directory/ai_birds.pid"
